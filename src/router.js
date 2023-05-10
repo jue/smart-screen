f1f8448d19bf4f4fabb949a85e9470f67cef60c1
xiangjianfeng <i@xiangjianfeng.com>
2023-05-10 15:05:48 +0800

    first commit

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import { useRoute, createRouter, createWebHistory } from 'vue-router'
+
+const defaultRouter = [
+  {
+    path: '/',
+    name: 'Home',
+    redirect: '/demo',
+    children: [
+      {
+        path: '/demo',
+        name: 'Demo',
+        component: () => import('@/views/screen/demo.vue'),
+        meta: { title: '智慧大屏' },
+      },
+    ],
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: defaultRouter,
+  scrollBehavior() {
+    return {
+      el: '#app',
+      top: 0,
+      behavior: 'smooth',
+    }
+  },
+})
+
+export default router
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+      // axios: '@/utils/request.js',
+    },
+  },
+  plugins: [vue()],
+})
